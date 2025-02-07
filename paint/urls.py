35dc28d6ea61180a2a9691e01bfad6832ea31962
agyesam12 <agyesam12@gmail.com>
2025-02-07 00:01:16 +0000

    added register urls and signout urls

--- paint/urls.py
+++ paint/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
+
 
 urlpatterns = [
     path('',views.home, name='home'),
     path('signin/', views.signin, name='signin'),
-    path('signout/', views.signout, name='signout')
+    path('signout/', views.signout, name='signout'),
+    path('register/', views.register, name='register'),
+    path('update_password/',auth_view.PasswordChangeView.as_view(template_name="update_password.html",success_url="/user_dashboard/" ),name='update_password'),
 ]

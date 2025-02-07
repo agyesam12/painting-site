988b6b8ccfd0337fbfb2491336b339629b7dce89
agyesam12 <agyesam12@gmail.com>
2025-02-07 00:00:41 +0000

    added  a usercreation form

--- paint/forms.py
+++ paint/forms.py
@@ -1,2 +1,19 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserRegistrationForm(UserCreationForm):
+    full_name = forms.CharField(max_length=60, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fieldname in ['full_name', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None

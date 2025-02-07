c9ecf63f588e372cf697eea839c87c0032bbfb50
agyesam12 <agyesam12@gmail.com>
2025-02-07 02:23:58 +0000

    added create service and portfolios urls path

--- paint/urls.py
+++ paint/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('register/', views.register, name='register'),
     path('update_password/',auth_view.PasswordChangeView.as_view(template_name="update_password.html",success_url="/user_dashboard/" ),name='update_password'),
-    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard')
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+     path('create_service/', views.create_service, name='create_service'),
+    path('create_portfolio/', views.create_portfolio, name='create_portfolio'),
+    path('create_testimonial/', views.create_testimonial, name='create_testimonial'),
 ]

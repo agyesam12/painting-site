7f23f45e8f35f40ecc870d50337a508b5c3285ce
agyesam12 <agyesam12@gmail.com>
2025-02-07 02:07:44 +0000

    added admin dashboard urls path

--- paint/urls.py
+++ paint/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('register/', views.register, name='register'),
     path('update_password/',auth_view.PasswordChangeView.as_view(template_name="update_password.html",success_url="/user_dashboard/" ),name='update_password'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard')
 ]

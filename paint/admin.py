1e974268e8f74c6124613b744d255633b99e1a15
agyesam12 <agyesam12@gmail.com>
2025-02-07 01:54:20 +0000

    registered the models in the admin dashboard

--- paint/admin.py
+++ paint/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(User),
+admin.site.register(Service),
+admin.site.register(Portfolio),
+admin.site.register(Testimonial),
+admin.site.register(ContactRequest),
+admin.site.register(EstimateRequest),
+admin.site.register(Notification)

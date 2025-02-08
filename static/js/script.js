13b7af2ee0a4f9603f2f834db862d97651c2c14c
agyesam12 <agyesam12@gmail.com>
2025-02-08 16:53:42 +0000

    updated the script.js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,1 @@
-// Handle form submission
-document
-  .getElementById("booking-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = {};
 
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    console.log("Form Submitted:", data);
-
-    // Here you would send the form data to an email or API.
-  });

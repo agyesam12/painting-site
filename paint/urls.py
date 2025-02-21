b04548661ae8eba8099fa2de94a43c27b5d33933
agyesam12 <agyesam12@gmail.com>
2025-02-21 22:06:20 +0000

    added user dashboard urls path

--- paint/urls.py
+++ paint/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('update_password/',auth_view.PasswordChangeView.as_view(template_name="update_password.html",success_url="/user_dashboard/" ),name='update_password'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('create_service/', views.create_service, name='create_service'),
     path('create_portfolio/', views.create_portfolio, name='create_portfolio'),
     path('create_testimonial/', views.create_testimonial, name='create_testimonial'),
